b06a0b9154853b877ef0d7f4a9d8ab7a28706aef
core-not-dumped <dltmd150722@gmail.com>
2022-11-10 21:48:21 +0900

    human GUI update quit, plus mode

--- test_with_GUI.py
+++ test_with_GUI.py
@@ -21,7 +21,7 @@ config = GoogleSnakeConfig(
     multi_channel=True,
     reward_mode='time_constrained',
     reward_scale=1,
-    n_foods=3, wall=True, portal=False, cheese=False, loop=False, reverse=False, moving=False,
+    n_foods=3, wall=True, portal=False, cheese=False, loop=False, reverse=True, moving=False,
     yinyang=False, key=False, box=False, poison=False, transparent=False, flag=False, slough=False,
     peaceful=False, mixed=False
 )
